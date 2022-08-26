d76fc358897da526c440c99e61beef72f7dc4b11
JNmeca <jorgenicolas0123@gmail.com>
2022-08-26 19:07:53 -0400

    26/08/2022

--- Liffy/Sources/main.c
+++ Liffy/Sources/main.c
@@ -41,7 +41,29 @@
 #include "IO_Map.h"
 
 /* User includes (#include below this line is not maintained by Processor Expert) */
+//DEFINE Valor de los Estados
+#define STATE_INIT				0	//Estado Inicial
+#define STATE_REVISION			1	//Estado Revisión
+#define STATE_MEASURE			2	//Estado Medición
+#define STATE_HEALTH			3	//Estado Salud Planta
+#define STATE_MAINTENANCE		4	//Estado Mantenimiento 
+#define STATE_ERROR				5	//Estado Error
 
+//Set
+#define ON_Relay 				1
+#define OFF_Relay				0
+
+//Prototipos de los Estados
+unsigned char F_STATE_INIT(void);			//Estado Inicial
+unsigned char F_STATE_REVISION(void);		//Estado Revisión
+unsigned char F_STATE_MEASURE(void);		//Estado Medición
+unsigned char F_STATE_HEALTH(void);			//Estado Salud Planta
+unsigned char F_STATE_MAINTENANCE(void);	//Estado Mantenimiento 
+unsigned char F_STATE_ERROR(void);			//Estado Error
+
+unsigned char ESTADO_ACTUAL = STATE_INIT;
+unsigned char ESTADO_SIGUIENTE = STATE_INIT;
+unsigned char ESTADO_PASADO = STATE_INIT;
 void main(void)
 {
   /* Write your local variable definition here */
@@ -52,7 +74,34 @@ void main(void)
 
   /* Write your code here */
   /* For example: for(;;) { } */
-
+  ESTADO_SIGUIENTE = F_STATE_INIT();
+  for (;;) 
+  {
+	if (ESTADO_SIGUIENTE == STATE_REVISION) 
+	{
+		ESTADO_SIGUIENTE = F_STATE_REVISION();
+	}
+	
+	if (ESTADO_SIGUIENTE == STATE_MEASURE) 
+	{
+		ESTADO_SIGUIENTE = F_STATE_MEASURE();	
+	}
+	
+	if (ESTADO_SIGUIENTE == STATE_HEALTH) 
+	{
+		ESTADO_SIGUIENTE = F_STATE_HEALTH();
+	}
+	
+	if (ESTADO_SIGUIENTE == STATE_MAINTENANCE) 
+	{
+		ESTADO_SIGUIENTE = F_STATE_MAINTENANCE();
+	}
+	
+	if (ESTADO_SIGUIENTE == STATE_ERROR) 
+	{
+		ESTADO_SIGUIENTE = F_STATE_ERROR();
+	}
+  }
   /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
   /*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
   #ifdef PEX_RTOS_START
@@ -76,3 +125,31 @@ void main(void)
 **
 ** ###################################################################
 */
+
+unsigned char F_STATE_INIT(void)		//Estado Inicial
+{
+	Relay1_PutVal(0);
+	
+}
+
+unsigned char F_STATE_REVISION(void)	//Estado Revisión
+{
+	
+}
+
+unsigned char F_STATE_MEASURE(void)		//Estado Medición
+{
+	
+}
+unsigned char F_STATE_HEALTH(void)		//Estado Salud Planta
+{
+		
+}
+unsigned char F_STATE_MAINTENANCE(void)	//Estado Mantenimiento 
+{
+	
+}
+unsigned char F_STATE_ERROR(void)		//Estado Error
+{
+	
+}

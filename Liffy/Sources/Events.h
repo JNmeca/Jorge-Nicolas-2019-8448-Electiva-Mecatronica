c2ac9aa9f84ecea8189c80dfec865f8c671b2da5
JNmeca <jorgenicolas0123@gmail.com>
2022-08-26 18:35:35 -0400

    26/08/2022

---
+++ Liffy/Sources/Events.h
@@ -0,0 +1,56 @@
+/* ###################################################################
+**     Filename    : Events.h
+**     Project     : Liffy
+**     Processor   : MC9S08MP16VLF
+**     Component   : Events
+**     Version     : Driver 01.02
+**     Compiler    : CodeWarrior HCS08 C Compiler
+**     Date/Time   : 2022-08-24, 00:13, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         No public methods
+**
+** ###################################################################*/
+/*!
+** @file Events.h
+** @version 01.02
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+
+#ifndef __Events_H
+#define __Events_H
+/* MODULE Events */
+
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+#include "Relay1.h"
+#include "Relay2.h"
+#include "Relay3.h"
+#include "Relay4.h"
+
+
+/* END Events */
+#endif /* __Events_H*/
+
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.3 [05.09]
+**     for the Freescale HCS08 series of microcontrollers.
+**
+** ###################################################################
+*/
